51649bf0ec72de4716b80d6e5941840ec9debf28
Richard Gigantone <rchrdgigantone@gmail.com>
2023-01-04 21:59:27 +0800

    Added show fn pwd information in admin panel

--- app/Http/Controllers/PwdController.php
+++ app/Http/Controllers/PwdController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Barangay;
 
 class PwdController extends Controller
 {
     public function listPWD(){
         $users = User::with('barangay')->where('type',false)->get();
         return view('admin.pwd',compact('users'));
+    }
+    public function show($id){
+        $brgy = Barangay::get();
+        $user = User::with('barangay')->where('type',false)->where('id',$id)->first();
+        if($user){
+            return view('admin.show-pwd',compact('user','brgy'));
+        }
+        return abort(404);
     }
 }
